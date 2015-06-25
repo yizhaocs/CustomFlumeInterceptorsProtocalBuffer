cf7c4a09f53741ba9062f0522581f8f98cf642d5
Yi Zhao <yizhao.cs@gmail.com>
2015-06-25 11:54:29 -0700

    first commit

---
+++ src/main/java/com/yizhao/flume/CustomInterceptor.java
@@ -0,0 +1,104 @@
+package com.yizhao.flume;
+
+import com.google.protobuf.ExtensionRegistry;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.yizhao.proto.KafkaProto.KafkaLoggingMessage;
+import com.yizhao.proto.TxnPayloadFriendProto.TxnPayloadFriend;
+import com.yizhao.proto.TxnResponseFriendProto.TxnResponseFriend;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+
+/**
+ * Created by yizhao on 6/18/15.
+ */
+public class CustomInterceptor
+        implements Interceptor {
+
+    private String moreBody;
+    private static ExtensionRegistry registry;
+
+
+    static {
+        registry = ExtensionRegistry.newInstance();
+        registry.add(TxnPayloadFriend.payload);
+        registry.add(TxnResponseFriend.response);
+    }
+
+
+    public CustomInterceptor(String moreBody) {
+        this.moreBody = moreBody;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Event intercept(Event event) {
+
+        // This is the event's body
+        ProtobufEncoder mProtobufEncoder = new ProtobufEncoder(null);
+        KafkaLoggingMessage decodeMessage = null;
+
+        try {
+            decodeMessage = KafkaLoggingMessage.parseFrom(event.getBody(), registry);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+
+        // These are the event's headers
+        Map<String, String> headers = event.getHeaders();
+
+
+        String newBody = moreBody ;
+        event.setBody(mProtobufEncoder.toBytes(decodeMessage));
+
+
+        // Let the enriched event go
+        return event;
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> events) {
+
+        List<Event> interceptedEvents =
+                new ArrayList<Event>(events.size());
+        for (Event event : events) {
+            // Intercept any event
+            Event interceptedEvent = intercept(event);
+            interceptedEvents.add(interceptedEvent);
+        }
+
+        return interceptedEvents;
+    }
+
+    @Override
+    public void close() {
+        // At interceptor shutdown
+    }
+
+    public static class Builder
+            implements Interceptor.Builder {
+
+        private String moreBody;
+
+        @Override
+        public void configure(Context context) {
+            // Retrieve property from flume conf
+            moreBody = context.getString("moreBody");
+        }
+
+        @Override
+        public Interceptor build() {
+            return new CustomInterceptor(moreBody);
+        }
+    }
+}
---
+++ src/main/java/com/yizhao/flume/ProtobufEncoder.java
@@ -0,0 +1,33 @@
+package com.yizhao.flume;
+
+/**
+ * Created by yizhao on 6/24/15.
+ */
+
+import kafka.serializer.Encoder;
+import kafka.utils.VerifiableProperties;
+import org.apache.log4j.Logger;
+
+import static com.yizhao.proto.KafkaProto.KafkaLoggingMessage;
+
+
+public class ProtobufEncoder implements Encoder<Object> {
+    private static final Logger LOGGER = Logger
+            .getLogger(ProtobufEncoder.class);
+
+    public ProtobufEncoder(VerifiableProperties verifiableProperties) {
+		/* This constructor must be present for successful compile. */
+    }
+
+    @Override
+    public byte[] toBytes(Object object) {
+        if (object == null) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("At ProtobufEncoder class, object is null");
+            }
+            return null;
+        }
+
+        return ((KafkaLoggingMessage) object).toByteArray();
+    }
+}
